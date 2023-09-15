f674c2da1482f66c89df9a2626f87d219d9ac5df
abhishek-pb1 <140099008+abhishek-pb1@users.noreply.github.com>
2023-09-15 13:15:32 +0530

    bug fix: can see the navbar moving up when the screen size is reduced on desktop

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const nav = document.querySelector("nav");
 const navLinks = document.querySelector(".nav-links");
 
 navBtn.addEventListener("click", () => {
+  navLinks.classList.add(".activated");
   const isExpanded = JSON.parse(navBtn.getAttribute("aria-expanded"));
   navBtn.setAttribute("aria-expanded", !isExpanded);
 });
